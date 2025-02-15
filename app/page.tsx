32b52a5718f5d945ff95ef4677e368ce0d22c57a
Aditya Girdhar <92713380+AdityaGirdhar@users.noreply.github.com>
2025-02-16 03:10:24 +0530

    Update layout and page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,37 @@
 'use client';
 
-import { useState } from 'react';
+import Image from 'next/image';
+import landing_background from '../public/landing_background.png';
+import logo from '../public/logo.png';
 
 export default function Home() {
-  const [repoUrl, setRepoUrl] = useState('');
-  const API_URL = 'http://localhost:8000';
-
-  console.log('API_URL:', API_URL);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(`${API_URL}/github/add-repos`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify( [repoUrl] ),
-      });
-      if (!response.ok) throw new Error('Failed to add repo');
-      console.log(response);
-      alert('Repository added successfully!');
-    } catch (error) {
-      console.error(error);
-      alert('Error adding repository');
-    }
-  };
-
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4">Add GitHub Repository</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <input
-          type="text"
-          value={repoUrl}
-          onChange={(e) => setRepoUrl(e.target.value)}
-          placeholder="Enter repository URL"
-          className="border p-2 rounded w-80 mb-2"
-          required
-        />
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-          Submit
-        </button>
-      </form>
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 ">
+      <Image
+        src={landing_background}
+        alt="Background"
+        fill
+        className="object-cover -z-10"
+        priority
+        quality={100}
+      />
+      <Image
+        src={logo}
+        alt="Logo"
+        width={150}
+        height={150}
+        className="object-cover absolute top-[40px]"
+        priority
+        quality={100}
+      />
+      <div>
+        <h1 className="text-4xl text-center text-white font-poppins">
+            <b>AI Agents made</b> <i className="font-thin">easy</i>
+        </h1>
+        <h1 className="text-2xl text-center text-white font-geistMono mt-3 italic">
+            coming soon
+        </h1>
+      </div>
     </main>
   );
 }
